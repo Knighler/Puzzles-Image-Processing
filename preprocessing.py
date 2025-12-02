0548f91cdcc15eebf425d7e56cc2ba5a099bc811
Youssef George <139137637+Knighler@users.noreply.github.com>
2025-12-02 20:57:38 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,20 +2,15 @@ import cv2
 import numpy as np
 import os
 
-
 INPUT_ROOT = 'E:\\ASU\\Fall 25\\Image\\Project\\Raw Images\\Gravity Falls'
 OUTPUT_ROOT = 'E:\\ASU\\Fall 25\\Image\\Project\\Raw Images\\Gravity Falls\\preprocessing_results'
 
 SUBFOLDERS = ['puzzle_2x2', 'puzzle_4x4', 'puzzle_8x8']
 
-
 def save_piece(image, folder_stage, subfolder, filename):
-
     target_dir = os.path.join(OUTPUT_ROOT, folder_stage, subfolder)
-
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-
     save_path = os.path.join(target_dir, filename)
     cv2.imwrite(save_path, image)
 
@@ -24,26 +19,21 @@ def slice_image_grid(image, grid_n):
     step_y = height // grid_n
     step_x = width // grid_n
     pieces = []
-
     for row in range(grid_n):
         for col in range(grid_n):
             y_start = row * step_y
             y_end = (row + 1) * step_y
             x_start = col * step_x
             x_end = (col + 1) * step_x
-
             piece = image[y_start:y_end, x_start:x_end]
             pieces.append(piece)
     return pieces
 
-
 def apply_denoising_NLM(piece):
     return cv2.fastNlMeansDenoisingColored(piece, None, 2, 1, 7, 21)
 
-
 def apply_denoising_gaussian(piece):
     return cv2.GaussianBlur(piece, (5, 5), 0)
-
 
 def apply_denoising_bilateral(piece):
     return cv2.bilateralFilter(piece, d=9, sigmaColor=75, sigmaSpace=75)
@@ -51,6 +41,16 @@ def apply_denoising_bilateral(piece):
 def apply_denoising_median(piece):
     return cv2.medianBlur(piece, 3)
 
+def apply_sharpening_laplacian(piece):
+    kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+    return cv2.filter2D(piece, -1, kernel)
+
+def apply_enhancement_HE(piece):
+    lab = cv2.cvtColor(piece, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+    l_eq = cv2.equalizeHist(l)
+    merged = cv2.merge((l_eq, a, b))
+    return cv2.cvtColor(merged, cv2.COLOR_LAB2BGR)
 
 def apply_enhancement(piece):
     lab_image = cv2.cvtColor(piece, cv2.COLOR_BGR2LAB)
@@ -78,7 +78,6 @@ def run_preprocessing():
                 number_str = before_x.split('_')[-1]
             else:
                 number_str = before_x
-
             grid_n = int(number_str)
         except:
             print(f"Error parsing size for {subfolder}")
@@ -94,11 +93,11 @@ def run_preprocessing():
                 continue
 
             base_name = os.path.splitext(file)[0]
-
             raw_pieces = slice_image_grid(img, grid_n)
 
             for idx, piece in enumerate(raw_pieces):
                 piece_name = f"{base_name}_piece_{idx}.png"
+                
                 save_piece(piece, "1_sliced", subfolder, piece_name)
 
                 denoised_piece = apply_denoising_NLM(piece)
@@ -106,6 +105,7 @@ def run_preprocessing():
 
                 enhanced_piece = apply_enhancement(piece)
                 save_piece(enhanced_piece, "3_enhanced", subfolder, piece_name)
+
 
             print(f"Processed {file} ,{len(raw_pieces)} pieces generated")
 
